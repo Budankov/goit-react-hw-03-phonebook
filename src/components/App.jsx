00fca03f74ad4674967591bf570f752bc008c240
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-01-30 14:45:09 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,14 +15,14 @@ class App extends Component {
 
   componentDidMount() {
     const contacts = JSON.parse(localStorage.getItem('contacts'));
-    if (contacts?.length) {
+    if (contacts) {
       this.setState({ contacts });
     }
   }
 
   componentDidUpdate(prevProps, prevState) {
     const { contacts } = this.state;
-    if (prevState.contacts.length !== contacts.length) {
+    if (prevState.contacts !== contacts) {
       localStorage.setItem('contacts', JSON.stringify(contacts));
     }
   }
